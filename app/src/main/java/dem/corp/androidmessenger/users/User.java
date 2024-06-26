9e130642ff31da5ef0e23200618868b9ad25333c
Sadvakas Daulet <drakuladragon240@gmail.com>
2024-06-26 20:39:05 +0500

    first commit

---
+++ app/src/main/java/dem/corp/androidmessenger/users/User.java
@@ -0,0 +1,11 @@
+package dem.corp.androidmessenger.users;
+
+public class User {
+    public String uid, username, profileImage;
+
+    public User(String uid, String username, String profileImage) {
+        this.uid = uid;
+        this.username = username;
+        this.profileImage = profileImage;
+    }
+}
